f86d7cba82fea9974f619383cfe11f870c354271
Ithyx <lamidey.m@gmail.com>
2020-08-01 01:16:07 -0400

    Abstracted Buffer layouts to make it easier to work with shaders. Class is probably very API agnostic.

--- src/Morrigu/Core/Application.cpp
+++ src/Morrigu/Core/Application.cpp
@@ -8,6 +8,34 @@ namespace MRG
 {
 	Application* Application::s_instance = nullptr;
 
+	// THIS FUNCTION NEEDS TO BE MOVED SOMEWHERE
+	// Where ? I have no idea :)
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+	{
+		// clang-format off
+		switch (type)
+		{
+			case MRG::ShaderDataType::Float:  return GL_FLOAT;
+			case MRG::ShaderDataType::Float2: return GL_FLOAT;
+			case MRG::ShaderDataType::Float3: return GL_FLOAT;
+			case MRG::ShaderDataType::Float4: return GL_FLOAT;
+
+			case MRG::ShaderDataType::Mat3:   return GL_FLOAT;
+			case MRG::ShaderDataType::Mat4:   return GL_FLOAT;
+
+			case MRG::ShaderDataType::Int:    return GL_INT;
+			case MRG::ShaderDataType::Int2:   return GL_INT;
+			case MRG::ShaderDataType::Int3:   return GL_INT;
+			case MRG::ShaderDataType::Int4:   return GL_INT;
+
+			case MRG::ShaderDataType::Bool:   return GL_BOOL;
+		}
+		// clang-format on
+
+		MRG_CORE_ASSERT(false, "Invalid shader data type ! ({})", type);
+		return 0;
+	}
+
 	Application::Application()
 	{
 		MRG_CORE_ASSERT(s_instance == nullptr, "Application already exists !");
@@ -22,40 +50,63 @@ namespace MRG
 		glGenVertexArrays(1, &m_vertexArray);
 		glBindVertexArray(m_vertexArray);
 
-		static constexpr float vertices[3 * 3] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f};
+		// clang-format off
+		static constexpr float vertices[3 * 7] =
+		{
+			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+			 0.5f, -0.5f, 0.0f, 0.2f, 0.2f, 0.8f, 1.0f,
+			 0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
+		};
 
 		m_vertexBuffer.reset(VertexBuffer::create(vertices, sizeof(vertices)));
 
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+		m_vertexBuffer->layout = {
+			{ShaderDataType::Float3, "a_Position"},
+			{ShaderDataType::Float4, "a_Color"}
+		};
+		// clang-format on
+
+		auto index = 0;
+		const auto& layout = m_vertexBuffer->layout;
+		for (const auto& element : layout) {
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index,
+			                      element.getComponentCount(),
+			                      ShaderDataTypeToOpenGLBaseType(element.type),
+			                      element.isNormalized ? GL_TRUE : GL_FALSE,
+			                      layout.getStride(),
+			                      (const void*)(element.offset));
+			++index;
+		}
 
 		unsigned int indices[3] = {0, 1, 2};
 		m_indexBuffer.reset(IndexBuffer::create(indices, 3));
 
 		std::string vertexShader = R"(
-			#version 460 core
+			#version 410 core
 
 			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
 
-			out vec3 v_Position;
+			out vec4 v_Color;
 
 			void main()
 			{
-				v_Position = a_Position;
+				v_Color = a_Color;
 				gl_Position = vec4(a_Position, 1.0);
 			}
 		)";
 
 		std::string fragmentShader = R"(
-			#version 460 core
+			#version 410 core
 
 			layout(location = 0) out vec4 color;
 			
-			in vec3 v_Position;
+			in vec4 v_Color;
 
 			void main()
 			{
-				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Color;
 			}
 		)";
 		m_shader.reset(new Shader(vertexShader, fragmentShader));
--- src/Morrigu/Renderer/Buffers.h
+++ src/Morrigu/Renderer/Buffers.h
@@ -1,10 +1,111 @@
 #ifndef MRG_CLASSES_BUFFERS
 #define MRG_CLASSES_BUFFERS
 
+#include "Core/Core.h"
+
 #include <cstdint>
 
 namespace MRG
 {
+	// clang-format off
+	enum class ShaderDataType
+	{
+		None = 0,
+		Float, Float2, Float3, Float4,
+		Mat3, Mat4,
+		Int, Int2, Int3, Int4,
+		Bool
+	};
+
+	[[nodiscard]] static uint32_t ShaderDataTypeSize(ShaderDataType type)
+	{
+		switch (type)
+		{
+		case ShaderDataType::Float:  return 4 * 1;
+		case ShaderDataType::Float2: return 4 * 2;
+		case ShaderDataType::Float3: return 4 * 3;
+		case ShaderDataType::Float4: return 4 * 4;
+
+		case ShaderDataType::Mat3:   return 4 * 3 * 3;
+		case ShaderDataType::Mat4:   return 4 * 4 * 4;
+
+		case ShaderDataType::Int:    return 4 * 1;
+		case ShaderDataType::Int2:   return 4 * 2;
+		case ShaderDataType::Int3:   return 4 * 3;
+		case ShaderDataType::Int4:   return 4 * 4;
+
+		case ShaderDataType::Bool:   return 1;
+		}
+
+		MRG_CORE_ASSERT(false, "Invalid shader data type ! ({})", type);
+		return 0;
+	}
+	// clang-format on
+
+	struct BufferElement
+	{
+		std::string name;
+		ShaderDataType type;
+		uint32_t size;
+		uint64_t offset;
+		bool isNormalized;
+
+		BufferElement() = default;
+		BufferElement(ShaderDataType type, const std::string& name, bool normalized = false)
+		    : name(name), type(type), size(ShaderDataTypeSize(type)), offset(0), isNormalized(normalized)
+		{}
+		[[nodiscard]] uint32_t getComponentCount() const
+		{
+			// clang-format off
+			switch (type)
+			{
+			case ShaderDataType::Float:  return 1;
+			case ShaderDataType::Float2: return 2;
+			case ShaderDataType::Float3: return 3;
+			case ShaderDataType::Float4: return 4;
+
+			case ShaderDataType::Mat3:   return 3 * 3;
+			case ShaderDataType::Mat4:   return 4 * 4;
+
+			case ShaderDataType::Int:    return 1;
+			case ShaderDataType::Int2:   return 2;
+			case ShaderDataType::Int3:   return 3;
+			case ShaderDataType::Int4:   return 4;
+
+			case ShaderDataType::Bool:   return 1;
+			}
+			// clang-format on
+
+			MRG_CORE_ASSERT(false, "Invalid shader data type ! ({})", type);
+			return 0;
+		}
+	};
+
+	class BufferLayout
+	{
+	public:
+		BufferLayout() = default;
+		BufferLayout(const std::initializer_list<BufferElement>& elements) : m_elements(elements)
+		{
+			uint64_t offset = 0;
+			for (auto& element : m_elements) {
+				element.offset = offset;
+				m_stride += element.size;
+				offset += element.size;
+			}
+		}
+
+		[[nodiscard]] inline auto getStride() const { return m_stride; }
+		[[nodiscard]] inline auto getElements() const { return m_elements; }
+
+		[[nodiscard]] inline auto begin() const { return m_elements.begin(); }
+		[[nodiscard]] inline auto end() const { return m_elements.end(); }
+
+	private:
+		std::vector<BufferElement> m_elements;
+		uint32_t m_stride = 0;
+	};
+
 	class VertexBuffer
 	{
 	public:
@@ -12,6 +113,8 @@ namespace MRG
 
 		virtual void bind() const = 0;
 		virtual void unbind() const = 0;
+
+		BufferLayout layout;
 
 		static VertexBuffer* create(const float* vertices, uint32_t size);
 	};
