84d5b9b62ffcbf15b6746f23b50fabb6a3dd30c3
Ithyx <lamidey.m@gmail.com>
2020-08-07 16:28:59 -0400

    Added keyCode definitions

---
+++ src/Morrigu/Core/KeyCodes.h
@@ -0,0 +1,133 @@
+#ifndef MRG_KEYCODES
+#define MEG_KEYCODES
+
+/* The unknown key */
+#define MRG_KEY_UNKNOWN -1
+
+/* Printable keys */
+#define MRG_KEY_SPACE 32
+#define MRG_KEY_APOSTROPHE 39 /* ' */
+#define MRG_KEY_COMMA 44      /* , */
+#define MRG_KEY_MINUS 45      /* - */
+#define MRG_KEY_PERIOD 46     /* . */
+#define MRG_KEY_SLASH 47      /* / */
+#define MRG_KEY_0 48
+#define MRG_KEY_1 49
+#define MRG_KEY_2 50
+#define MRG_KEY_3 51
+#define MRG_KEY_4 52
+#define MRG_KEY_5 53
+#define MRG_KEY_6 54
+#define MRG_KEY_7 55
+#define MRG_KEY_8 56
+#define MRG_KEY_9 57
+#define MRG_KEY_SEMICOLON 59 /* ; */
+#define MRG_KEY_EQUAL 61     /* = */
+#define MRG_KEY_A 65
+#define MRG_KEY_B 66
+#define MRG_KEY_C 67
+#define MRG_KEY_D 68
+#define MRG_KEY_E 69
+#define MRG_KEY_F 70
+#define MRG_KEY_G 71
+#define MRG_KEY_H 72
+#define MRG_KEY_I 73
+#define MRG_KEY_J 74
+#define MRG_KEY_K 75
+#define MRG_KEY_L 76
+#define MRG_KEY_M 77
+#define MRG_KEY_N 78
+#define MRG_KEY_O 79
+#define MRG_KEY_P 80
+#define MRG_KEY_Q 81
+#define MRG_KEY_R 82
+#define MRG_KEY_S 83
+#define MRG_KEY_T 84
+#define MRG_KEY_U 85
+#define MRG_KEY_V 86
+#define MRG_KEY_W 87
+#define MRG_KEY_X 88
+#define MRG_KEY_Y 89
+#define MRG_KEY_Z 90
+#define MRG_KEY_LEFT_BRACKET 91  /* [ */
+#define MRG_KEY_BACKSLASH 92     /* \ */
+#define MRG_KEY_RIGHT_BRACKET 93 /* ] */
+#define MRG_KEY_GRAVE_ACCENT 96  /* ` */
+#define MRG_KEY_WORLD_1 161      /* non-US #1 */
+#define MRG_KEY_WORLD_2 162      /* non-US #2 */
+
+/* Function keys */
+#define MRG_KEY_ESCAPE 256
+#define MRG_KEY_ENTER 257
+#define MRG_KEY_TAB 258
+#define MRG_KEY_BACKSPACE 259
+#define MRG_KEY_INSERT 260
+#define MRG_KEY_DELETE 261
+#define MRG_KEY_RIGHT 262
+#define MRG_KEY_LEFT 263
+#define MRG_KEY_DOWN 264
+#define MRG_KEY_UP 265
+#define MRG_KEY_PAGE_UP 266
+#define MRG_KEY_PAGE_DOWN 267
+#define MRG_KEY_HOME 268
+#define MRG_KEY_END 269
+#define MRG_KEY_CAPS_LOCK 280
+#define MRG_KEY_SCROLL_LOCK 281
+#define MRG_KEY_NUM_LOCK 282
+#define MRG_KEY_PRINT_SCREEN 283
+#define MRG_KEY_PAUSE 284
+#define MRG_KEY_F1 290
+#define MRG_KEY_F2 291
+#define MRG_KEY_F3 292
+#define MRG_KEY_F4 293
+#define MRG_KEY_F5 294
+#define MRG_KEY_F6 295
+#define MRG_KEY_F7 296
+#define MRG_KEY_F8 297
+#define MRG_KEY_F9 298
+#define MRG_KEY_F10 299
+#define MRG_KEY_F11 300
+#define MRG_KEY_F12 301
+#define MRG_KEY_F13 302
+#define MRG_KEY_F14 303
+#define MRG_KEY_F15 304
+#define MRG_KEY_F16 305
+#define MRG_KEY_F17 306
+#define MRG_KEY_F18 307
+#define MRG_KEY_F19 308
+#define MRG_KEY_F20 309
+#define MRG_KEY_F21 310
+#define MRG_KEY_F22 311
+#define MRG_KEY_F23 312
+#define MRG_KEY_F24 313
+#define MRG_KEY_F25 314
+#define MRG_KEY_KP_0 320
+#define MRG_KEY_KP_1 321
+#define MRG_KEY_KP_2 322
+#define MRG_KEY_KP_3 323
+#define MRG_KEY_KP_4 324
+#define MRG_KEY_KP_5 325
+#define MRG_KEY_KP_6 326
+#define MRG_KEY_KP_7 327
+#define MRG_KEY_KP_8 328
+#define MRG_KEY_KP_9 329
+#define MRG_KEY_KP_DECIMAL 330
+#define MRG_KEY_KP_DIVIDE 331
+#define MRG_KEY_KP_MULTIPLY 332
+#define MRG_KEY_KP_SUBTRACT 333
+#define MRG_KEY_KP_ADD 334
+#define MRG_KEY_KP_ENTER 335
+#define MRG_KEY_KP_EQUAL 336
+#define MRG_KEY_LEFT_SHIFT 340
+#define MRG_KEY_LEFT_CONTROL 341
+#define MRG_KEY_LEFT_ALT 342
+#define MRG_KEY_LEFT_SUPER 343
+#define MRG_KEY_RIGHT_SHIFT 344
+#define MRG_KEY_RIGHT_CONTROL 345
+#define MRG_KEY_RIGHT_ALT 346
+#define MRG_KEY_RIGHT_SUPER 347
+#define MRG_KEY_MENU 348
+
+#define MRG_KEY_LAST MRG_KEY_MENU
+
+#endif
