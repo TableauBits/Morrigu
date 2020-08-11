fbeb47275edf54376c0bf174aab2759db8818a89
Ithyx <lamidey.m@gmail.com>
2020-08-10 21:58:28 -0400

    Basic window resizing implemented

--- src/Morrigu/Renderer/APIs/OpenGL/RenderingAPI.cpp
+++ src/Morrigu/Renderer/APIs/OpenGL/RenderingAPI.cpp
@@ -10,6 +10,7 @@ namespace MRG::OpenGL
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	}
 
+	void RenderingAPI::setViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height) { glViewport(x, y, width, height); }
 	void RenderingAPI::setClearColor(const glm::vec4& color) { glClearColor(color.r, color.g, color.b, color.a); }
 	void RenderingAPI::clear() { glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); }
 
--- src/Morrigu/Renderer/APIs/OpenGL/RenderingAPI.h
+++ src/Morrigu/Renderer/APIs/OpenGL/RenderingAPI.h
@@ -10,6 +10,7 @@ namespace MRG::OpenGL
 	public:
 		void init() override;
 
+		void setViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height) override;
 		void setClearColor(const glm::vec4& color) override;
 		void clear() override;
 
