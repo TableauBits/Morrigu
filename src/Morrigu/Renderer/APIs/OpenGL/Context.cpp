e2d5401e1e5562e55026ab5486cdd313093debca
Ithyx <lamidey.m@gmail.com>
2020-08-11 22:59:59 -0400

    Added debug assertion to ensure OpenGL version is sufficient

--- src/Morrigu/Renderer/APIs/OpenGL/Context.cpp
+++ src/Morrigu/Renderer/APIs/OpenGL/Context.cpp
@@ -18,7 +18,15 @@ namespace MRG::OpenGL
 		MRG_ENGINE_INFO("\tHardware vendor: {0}", glGetString(GL_VENDOR));
 		MRG_ENGINE_INFO("\tHardware device used to render: {0}", glGetString(GL_RENDERER));
 		MRG_ENGINE_INFO("\tOpenGL version: {0}", glGetString(GL_VERSION));
+
+#ifndef NDEBUG
+		int vMajor, vMinor;
+		glGetIntegerv(GL_MAJOR_VERSION, &vMajor);
+		glGetIntegerv(GL_MINOR_VERSION, &vMinor);
+
+		MRG_CORE_ASSERT((vMajor > 4 || (vMajor == 4 && vMinor >= 5)), "OpenGL 4.5 required !")
+#endif
 	}
 
 	void Context::swapBuffers() { glfwSwapBuffers(m_window); }
-}  // namespace MRG
+}  // namespace MRG::OpenGL
