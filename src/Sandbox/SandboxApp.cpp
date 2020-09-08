925161bea898d09f5a1866391f92dd02e36609de
Ithyx <lamidey.m@gmail.com>
2020-09-08 00:52:46 -0400

    Created Sandox layer to test new vulkan impl

--- src/Sandbox/SandboxApp.cpp
+++ src/Sandbox/SandboxApp.cpp
@@ -1,9 +1,20 @@
 #include "Sandbox2D.h"
 
+class VulkanSandbox : public MRG::Layer
+{
+public:
+	VulkanSandbox() : Layer("Vulkan layer") {}
+
+	void onAttach() override {}
+	void onDetach() override {}
+	void onUpdate(MRG::Timestep) override {}
+	void onEvent(MRG::Event&) override {}
+};
+
 class Sandbox : public MRG::Application
 {
 public:
-	Sandbox() { pushLayer(new Sandbox2D{}); }
+	Sandbox() { pushLayer(new VulkanSandbox{}); }
 	~Sandbox() {}
 };
 
