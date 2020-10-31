5cba90805b48fa2b0e85d47fd2e71bc2b0fb4a1f
Ithyx <lamidey.m@gmail.com>
2020-10-30 21:23:38 -0400

    Made Vulkan create normalised color space textures I don't know if this is correct, it's only for GL parity at the moment.

--- src/Morrigu/Renderer/APIs/Vulkan/Textures.cpp
+++ src/Morrigu/Renderer/APIs/Vulkan/Textures.cpp
@@ -47,7 +47,7 @@ namespace MRG::Vulkan
 		            windowData->device,
 		            m_width,
 		            m_height,
-		            VK_FORMAT_R8G8B8A8_SRGB,
+		            VK_FORMAT_R8G8B8A8_UNORM,
 		            VK_IMAGE_TILING_OPTIMAL,
 		            VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT,
 		            VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
@@ -55,18 +55,18 @@ namespace MRG::Vulkan
 		            m_memoryHandle);
 
 		transitionImageLayout(
-		  windowData, m_imageHandle, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
+		  windowData, m_imageHandle, VK_FORMAT_R8G8B8A8_UNORM, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
 		copyBufferToImage(windowData, stagingBuffer.handle, m_imageHandle, m_width, m_height);
 		transitionImageLayout(windowData,
 		                      m_imageHandle,
-		                      VK_FORMAT_R8G8B8A8_SRGB,
+		                      VK_FORMAT_R8G8B8A8_UNORM,
 		                      VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
 		                      VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
 
 		vkDestroyBuffer(windowData->device, stagingBuffer.handle, nullptr);
 		vkFreeMemory(windowData->device, stagingBuffer.memoryHandle, nullptr);
 
-		m_imageView = createImageView(windowData->device, m_imageHandle, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_ASPECT_COLOR_BIT);
+		m_imageView = createImageView(windowData->device, m_imageHandle, VK_FORMAT_R8G8B8A8_UNORM, VK_IMAGE_ASPECT_COLOR_BIT);
 
 		VkSamplerCreateInfo samplerInfo{};
 		samplerInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
@@ -136,7 +136,7 @@ namespace MRG::Vulkan
 		            windowData->device,
 		            m_width,
 		            m_height,
-		            VK_FORMAT_R8G8B8A8_SRGB,
+		            VK_FORMAT_R8G8B8A8_UNORM,
 		            VK_IMAGE_TILING_OPTIMAL,
 		            VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT,
 		            VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
@@ -144,18 +144,18 @@ namespace MRG::Vulkan
 		            m_memoryHandle);
 
 		transitionImageLayout(
-		  windowData, m_imageHandle, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
+		  windowData, m_imageHandle, VK_FORMAT_R8G8B8A8_UNORM, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
 		copyBufferToImage(windowData, stagingBuffer.handle, m_imageHandle, m_width, m_height);
 		transitionImageLayout(windowData,
 		                      m_imageHandle,
-		                      VK_FORMAT_R8G8B8A8_SRGB,
+		                      VK_FORMAT_R8G8B8A8_UNORM,
 		                      VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
 		                      VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
 
 		vkDestroyBuffer(windowData->device, stagingBuffer.handle, nullptr);
 		vkFreeMemory(windowData->device, stagingBuffer.memoryHandle, nullptr);
 
-		m_imageView = createImageView(windowData->device, m_imageHandle, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_ASPECT_COLOR_BIT);
+		m_imageView = createImageView(windowData->device, m_imageHandle, VK_FORMAT_R8G8B8A8_UNORM, VK_IMAGE_ASPECT_COLOR_BIT);
 
 		VkSamplerCreateInfo samplerInfo{};
 		samplerInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
